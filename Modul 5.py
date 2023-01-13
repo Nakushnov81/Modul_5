6d0f54d4fc8c4db25792e3f7bc6020e5f149948d
Nakushnov81 <nakushnov@rambler.ru>
2023-01-13 16:15:29 +0400

    Задание 1

--- Modul 5.py
+++ Modul 5.py
@@ -1,16 +1,14 @@
 class StringVar:
-    x = ''
+
     def set_srt(self, x):
         self.x = x
         return x
 
-    def get_str(self, x):
-        return x
+    def get_str(self):
+        return print(self.x)
 
 st = StringVar()
 
 st.set_srt(input('Введите строку: '))
-print(st.__dict__)
 
-st.get_str(input('Внесите изменения: '))
-print(st.__dict__)
+st.get_str()
