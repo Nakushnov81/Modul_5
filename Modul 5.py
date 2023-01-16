6dda6c2c337456385c97c5481a42bd63bb4a5753
Nakushnov81 <nakushnov@rambler.ru>
2023-01-16 11:28:16 +0400

    Задание 2

--- Modul 5.py
+++ Modul 5.py
@@ -16,3 +16,29 @@ st.get_str()
 st.set_srt(input('Измените строку: '))
 
 st.get_str()
+
+
+class Point:
+    color = 'red'
+    size = 2
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    def get_coord(self):
+        return print(self.x, self.y)
+
+    def set_coord(self, x, y):
+        self.x = x
+        self.y = y
+        return x, y
+
+pt = Point(2, 3)
+
+pt.get_coord()
+
+pt.set_coord(5, 7)
+
+pt.get_coord()
+
+
