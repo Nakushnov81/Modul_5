62a8cb9c01378d3ea9739a3be952d54abff4daba
Nakushnov Anatoliy <nakushnov@rambler.ru>
2023-01-23 12:07:58 +0400

    Задание 3(1)

--- Modul 5.py
+++ Modul 5.py
@@ -18,6 +18,7 @@ st.set_srt(input('Измените строку: '))
 st.get_str()
 
 
+
 class Point:
     color = 'red'
     size = 2
@@ -42,22 +43,19 @@ pt.set_coord(5, 7)
 pt.get_coord()'''
 
 
-class Warrior:
-    health = 100
-    # armor =
-
-    def attack(self):
-        pass
-
-    def deffence(self):
-        pass
-
 from random import randint
 
-w1 = Warrior
-w2 = Warrior
+class Warrior:
+    def __init__(self, health = 100):
+        self.health = health
 
-while w1.health > 10 or w2.health > 10:
+
+
+
+w1 = Warrior()
+w2 = Warrior()
+
+while w1.health > 10 and w2.health > 10:
     x = randint(0, 1)
     print(x)
     if x == 0:
@@ -65,9 +63,10 @@ while w1.health > 10 or w2.health > 10:
         print(f'Здоровье война 1: {w1.health},\nЗдоровье война 2: {w2.health}')
     else:
         w2.health -= randint(1, 10)
-        print(f'Здоровье война 2: {w2.health},\nЗдоровье война 1: {w1.health}')
+        print(f'Здоровье война 1: {w1.health},\nЗдоровье война 2: {w2.health}')
 
 if w1.health < 10:
     print('Воин 2 победил')
 else:
     print('Воин 1 победил')
+
